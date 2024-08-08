035a23ce8f5fe923246883829ea5d11483bf584e
wlals <jiminelp@gmail.com>
2024-08-08 23:37:43 +0900

    burn :: 의존성 제거

--- packages/EntryDesignSystem/src/components/Spinner.tsx
+++ packages/EntryDesignSystem/src/components/Spinner.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from '@emotion/styled';
 import { green100, green500, orange100, orange500 } from '../style/color';
 import { marginCssType, marginToCss } from '../utils/margin';
--- packages/EntryDesignSystem/src/style/icon/ExclamationMark.tsx
+++ packages/EntryDesignSystem/src/style/icon/ExclamationMark.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ExclamationMark = () => {
     return (
         <svg
