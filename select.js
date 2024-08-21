6c4ec3fd6dd8cbcd34f193cbdb66538bc206a07e
dutexion <dutexion@gmail.com>
2024-08-21 16:19:30 +0900

    fix :: conflict

--- select.js
+++ select.js
@@ -7,6 +7,7 @@ const questions = [
     name: 'file',
     message: 'Whats the project you want to run?:',
     choices: [
+      'Entry-LTS',
       'Admission-Admin-LTS',
       'Admission-LTS',
       'Entry-Auth',
@@ -30,6 +31,9 @@ inquirer.prompt(questions).then((answers) => {
   } else if (answers.file === 'Admission-Admin-LTS') {
     command = 'yarn';
     args = ['workspace', '@entrydsm/admin', 'run', 'dev'];
+  } else if (answers.file === 'Entry-LTS') {
+    command = 'yarn';
+    args = ['workspace', '@entrydsm/main', 'run', 'start'];
   }
 
   const child = spawn(command, args, { shell: true });
