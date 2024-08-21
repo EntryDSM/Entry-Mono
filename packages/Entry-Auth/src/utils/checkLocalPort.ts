c241b321819e4a64e6ddb43d14bab9363cadbdb9
ojinikim <ojinikim@gmail.com>
2024-08-21 19:20:25 +0900

    del :: oauth delete

--- packages/Entry-Auth/src/utils/checkLocalPort.ts
+++
@@ -1,8 +0,0 @@
-export const checkLocalPort = async (port: number): Promise<boolean> => {
-  try {
-    const response = await fetch(`http://localhost:${port}`);
-    return response.status === 200;
-  } catch (error) {
-    return false;
-  }
-};
