22766c30eb40dbbe6388755189e42831e0108f6b
ojinikim <ojinikim@gmail.com>
2024-09-20 10:52:44 +0900

    chore :: 확인용 test

--- packages/Entry-LTS/src/constant/env.ts
+++ packages/Entry-LTS/src/constant/env.ts
@@ -10,7 +10,9 @@ const isStag = window.location.href.includes('stag');
 // test
 export const COOKIE_DOMAIN =
   process.env.NODE_ENV === 'development' ? 'localhost' : 'entrydsm.hs.kr';
-console.log(COOKIE_DOMAIN);
+console.log('쿠키 도메인: ', COOKIE_DOMAIN);
+console.log('삼항연산자 확인:', process.env.NODE_ENV === 'development');
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 export const MAIN_URL = import.meta.env.VITE_MAIN_URL;
 
