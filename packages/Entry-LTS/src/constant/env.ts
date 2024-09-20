d35e99fa7700169af023d18fca9bde736faa2b29
ojinikim <ojinikim@gmail.com>
2024-09-20 11:19:46 +0900

    chore :: 원상복구

--- packages/Entry-LTS/src/constant/env.ts
+++ packages/Entry-LTS/src/constant/env.ts
@@ -3,16 +3,9 @@ import process from 'process';
 const isLocalhost = window.location.href.includes('localhost');
 const isStag = window.location.href.includes('stag');
 
-// export const COOKIE_DOMAIN = window.location.href.includes('localhost')
-//   ? 'localhost'
-//   : 'entrydsm.hs.kr';
-
-// test
-export const COOKIE_DOMAIN =
-  process.env.NODE_ENV === 'development' ? 'localhost' : 'entrydsm.hs.kr';
-console.log('쿠키 도메인: ', COOKIE_DOMAIN);
-console.log('삼항연산자 확인:', process.env.NODE_ENV === 'development');
-console.log('NODE_ENV:', process.env.NODE_ENV);
+export const COOKIE_DOMAIN = window.location.href.includes('localhost')
+  ? 'localhost'
+  : 'entrydsm.hs.kr';
 
 export const MAIN_URL = import.meta.env.VITE_MAIN_URL;
 
